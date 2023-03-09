bb54139396d09a5a5793891b2ddaca821357d662
flowersin <eschaefer7d4@gmail.com>
2023-03-09 11:21:44 -0700

    renamed:    PPr3/3-1.py -> Ch3/PPr3/3-1.py 	new file:   Ch3/PPr3/3-2.py 	new file:   Ch3/PPr3/3-3.py 	new file:   Ch3/PPr3/3-5.py

---
+++ Ch3/PPr3/3-3.py
@@ -0,0 +1,8 @@
+password = input('Enter the password: ')
+
+if password == 'prospero':
+    print('Password accepted!')
+
+else:
+    print('Sorry, wrong password')
+    
---
+++ Ch3/PPr3/3-5.py
@@ -0,0 +1,14 @@
+MIN_SALARY = 30000
+MIN_YEARS = 2
+
+salary = float(input('Enter your annual slary: '))
+years_on_job = int(input('Enter the number of years employed: '))
+
+if salary >= MIN_SALARY:
+    if years_on_job >= MIN_YEARS:
+        print('You qualify for the loan.')
+    else:
+        print('You must have been employed for ', MIN_YEARS, ' to qualify.')
+else:
+    print('You earn at least $', format(MIN_SALARY, '.2f'), ' per year to qualify')
+
